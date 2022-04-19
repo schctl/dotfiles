f19c2c99f658fc938b7fc4c0dc889f97a836fbf3
Sachin Cherian <sachinctl@protonmail.com>
2022-04-19 14:01:33 +0530

    housekeeping

--- i3/scripts/exit.sh
+++ i3/scripts/exit.sh
@@ -5,7 +5,7 @@ set -e
 chosen="$(rofi -dmenu\
 		-i\
 		-no-fixed-num-lines\
-		-p "Do you really want to exit i3? : "\
+		-p "Exit i3? : "\
 		-theme ~/.config/rofi/exit-i3.rasi | tr '[:upper:]' '[:lower:]')"
 
 case $chosen in
