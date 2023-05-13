cc8112e09f04aa60df3be8e5c50860312ad7d1a9
Sachin Cherian <sachinctl@protonmail.com>
2023-05-13 23:31:59 +0530

    update `ls-installed` function

--- dotfiles/fish/functions/ls-installed.fish
+++ dotfiles/fish/functions/ls-installed.fish
@@ -1,4 +1,4 @@
 function ls-installed --description="convenience function to list all explicitly installed packages"
-  comm -23 (pacman -Qqett | sort | psub) (pacman -Qqg base-devel | sort | uniq | psub) $argv
+  comm -23 (pacman -Qqett | sort | psub) (pacman -Qqg xorg | sort | uniq | psub) $argv
         
 end
